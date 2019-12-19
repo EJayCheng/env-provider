d5e3d038d9d82d6fd04f1b65f011de155a5676b3
dweizhe <d.weizhe@anyong.com.tw>
2019-12-19 13:44:15 +0800

    feat(env): support browse

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { safeJsonParse } from "@ay/util";
 
+const env = process ? process.env : window["env"];
+
 export function int(key: string, defaultValue: any = undefined): number {
-  const value = parseInt(process.env[key]);
+  const value = parseInt(env[key]);
   if (isNaN(value)) {
     return defaultValue;
   }
@@ -9,15 +11,15 @@ export function int(key: string, defaultValue: any = undefined): number {
 }
 
 export function str(key: string, defaultValue: any = undefined): string {
-  return process.env[key] || defaultValue;
+  return env[key] || defaultValue;
 }
 
 export function bool(key: string, defaultValue: any = undefined): boolean {
-  const value = (process.env[key] + "").toUpperCase();
+  const value = (env[key] + "").toUpperCase();
   if (value === "") return defaultValue;
   return ["YES", "1", "TRUE", "ON", "Y", "O", "T"].includes(value);
 }
 
 export function json(key: string, defaultValue: any = null): any {
-  return safeJsonParse(process.env[key], defaultValue);
+  return safeJsonParse(env[key], defaultValue);
 }
